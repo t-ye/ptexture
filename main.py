f9a0d8ea9e31ffb01203501bec44f64ae892d326
t-ye <lightningleaf0@gmail.com>
2018-12-12 02:08:48 -0800

    Move to main

--- main.py
+++ main.py
@@ -1,375 +1,81 @@
-from __future__ import annotations
 from PyQt5 import QtWidgets, QtGui, QtCore
-import collections
+import qt_ext
+import ptexture
+import npqt
+import functional
+
 
 QtCore.pyqtRemoveInputHook() # remove this when done debugging
-
-if __name__ == '__main__' :
-	app = QtWidgets.QApplication([])
-
-def screen_resolution() :
-	global app
-
-	rect = app.desktop().screenGeometry()
-	width, height = rect.width(), rect.height()
-	return (width, height)
-
-import npqt
-import ptexture
 
 class Main(QtWidgets.QMainWindow) :
 
 	def __init__(self) :
 
 		super().__init__()
-		self.ptextures_dict = collections.defaultdict(dict)
-		self.curr_idx = None
 
-		self.ptextures = list()
+		self.centralWidget = QtWidgets.QFrame()
+		self.setCentralWidget(self.centralWidget)
 
-		self.createWidgets()
-		self.setupLayout()
-		self.createPixmaps()
+		self.createDisplay()
+		self.createParamGui()
+		self.createLayout()
+		self.populateLayout()
+
+		self.attachSignals_paramGui()
 
 		self.show()
-		#self.setWindowState(QtCore.Qt.WindowMaximized) doesn't work with X11
 
-	def addpTexture(self, ptexture : ptexture.ptexture) :
 
-		# 1-to-1 correspondence between lsit of ptextures and comboBox items
-		self.ptextures.append(ptexture)
-		# TODO : move comboBox to side.
-		self.comboBox.addItem(ptexture.name)
+	def createDisplay(self) :
+		self.textureDisplay = QtWidgets.QLabel(self)
+		self.textureDisplay.setScaledContents(True) # auto resize
 
-	def updateTexture(self, idx) :
+	def setDisplay(self, pixmap : QtGui.QPixmap) :
 
-		# if index has not changed, don't attempt to change the texture
-		if self.curr_idx == idx :
+		self.textureDisplay.setPixmap(pixmap)
+
+	def updateDisplay(self) :
+
+		try :
+			tex = self.paramGui_dict.get()(**self.paramGui_dict.getDict()['texture'])
+		except ValueError as ve :
+			print(ve)
 			return
 
-		# if comboBox's index doesn't match, change it so it does
-			# comboBox should have a listener so that it calls updateTexture again,
-		# so return immediately after
-		if self.comboBox.currentIndex() != idx :
-			self.comboBox.setCurrentIndex(idx)
-			return
+		im = ptexture.textureToImage(tex)
+		pixmap = QtGui.QPixmap.fromImage(im)
+		self.setDisplay(pixmap)
 
-		# update index into comboBox and ptextures list
-		self.curr_idx = idx
+	def createParamGui(self) :
 
-		texture = self.ptextures[idx]
+		self.param = functional.StringParameter('texture',
+			choices    = tuple(ptexture.ptexture.instances.keys()),
+			parse      = lambda choice : ptexture.ptexture.instances[choice],
+			get_params = lambda ptex : ptex.params)
 
-		# 1-to-1 correspondence between ptextures_dict a vlayout
-		self.ptextures_dict.clear()
+		self.paramGui_frame = QtWidgets.QFrame(self)
+		self.paramGui_layout = QtWidgets.QVBoxLayout(self.paramGui_frame)
 
-		# clear vlayout
-		for i in reversed(range(self.vlayout.count())):
-			widget = self.vlayout.itemAt(i).widget()
-			self.vlayout.removeWidget(widget)
-			widget.setParent(None)
-			widget.deleteLater()
+		self.paramGui_dict = qt_ext.QNamedDict(self.param, self.paramGui_frame)
+		self.paramGui_updateButton = QtWidgets.QPushButton('Update')
 
+		self.paramGui_layout.addWidget(self.paramGui_dict)
+		self.paramGui_layout.addWidget(self.paramGui_updateButton)
 
-		import PyQt5_ext
 
-		def add_param_gui(tex) :
-			self.param_gui = PyQt5_ext.ptexture_params_gui(tex)
-			self.vlayout.addWidget(self.param_gui)
+	def attachSignals_paramGui(self) :
 
+		self.paramGui_updateButton.clicked.connect(self.updateDisplay)
 
-		def update() :
 
-			self.ptextures_dict = self.param_gui.get()
+	def createLayout(self) :
+		self.layout = QtWidgets.QHBoxLayout(self.centralWidget)
 
-			self.updateImage()
-
-		add_param_gui(texture)
-
-		# send GUI params to dict with this button
-		button = QtWidgets.QPushButton('Update')
-		button.clicked.connect(update)
-		self.vlayout.addWidget(button)
-
-		update()
-
-		self.updateImage()
-
-
-	def updateImage(self) :
-		im = npqt.arr_to_image(*self.ptextures[self.curr_idx](**self.ptextures_dict))
-		pm = QtGui.QPixmap(im)
-		self.label.setPixmap(pm)
-
-	def createPixmaps(self) :
-
-		noise = ptexture.get_noise()
-		self.addpTexture(noise)
-		self.addpTexture(ptexture.get_zs(noise))
-
-	def createWidgets(self) :
-		self.vlayout = None
-		self.label = QtWidgets.QLabel()
-		self.label.setScaledContents(True) # auto resize
-
-		self.next = QtWidgets.QPushButton('next')
-		self.next.clicked.connect(lambda :
-			self.comboBox.setCurrentIndex((self.comboBox.currentIndex() + 1) %
-		                                self.comboBox.count()
-			)
-		)
-		self.prev = QtWidgets.QPushButton('prev')
-		self.prev.clicked.connect(lambda :
-			self.comboBox.setCurrentIndex((self.comboBox.currentIndex() - 1) %
-		                                self.comboBox.count()
-			)
-		)
-		self.comboBox = QtWidgets.QComboBox(self)
-		self.comboBox.currentIndexChanged.connect(self.updateTexture)
-
-	def setupLayout(self) :
-		# setup mainLayout
-		widget = QtWidgets.QWidget()
-		widget3 = QtWidgets.QWidget()
-		hlayout = QtWidgets.QHBoxLayout(widget)
-		self.mainLayout = hlayout
-		#self.setLayout(self.mainLayout)
-		self.setCentralWidget(hlayout.parent())
-
-		# add to mainLayout
-
-
-		# changer
-		widget2 = QtWidgets.QWidget()
-		changer = QtWidgets.QHBoxLayout(widget2)
-		changer.addWidget(self.prev)
-		changer.addWidget(self.comboBox)
-		changer.addWidget(self.next)
-
-		# changer + image
-		vlayout = QtWidgets.QVBoxLayout(widget3)
-		vlayout.addWidget(changer.parent())
-		vlayout.addWidget(self.label)
-
-		hlayout.addWidget(widget3)
-
-		container = QtWidgets.QWidget()
-		self.vlayout = QtWidgets.QVBoxLayout(container)
-		self.mainLayout.addWidget(container)
-
-
-def blur(**kwargs) :
-
-	base = kwargs['base']
-	R,C=base.shape[:2]
-	fmt = kwargs['fmt']
-	# get matrix in middle same as base
-	# but with a wraparound border around it
-	repborder = np.tile(np.atleast_3d(base), (3,3,1))[R-1:2*R+1,C-1:2*C+1]
-	#print(base.shape)
-	#print(repborder.shape)
-
-	# get the matrices that are a result of
-	# moving the central instance of base left, right down, and up cast to 16 bits
-	# to prevent later addition overflow
-	l = repborder[1:R+1,:C].astype(np.uint16)
-	r = repborder[1:R+1:,2:]
-	b = repborder[2:,1:C+1]
-	t = repborder[:R,1:C+1]
-
-	#print(l.shape)
-	#print(r.shape)
-	#print(b.shape)
-	#print(t.shape)
-
-	return ((l+r+b+t+np.atleast_3d(base))/5, fmt)
-
-def zoomed_smooth_noise(**kwargs) :
-
-	base = kwargs['base']
-	zoom = kwargs['zoom']
-	fmt = kwargs['fmt']
-	# m assumed 2D
-
-	from time import time
-
-	t = time()
-
-
-	# get ranges corresonding to the top left (1/zoom)th
-	# portion of the matrix
-	(xf, yf), i = np.modf(np.indices(base.shape[:2]) / zoom)
-	x, y = i.astype(np.int)
-	print(time() - t)
-
-	# up, left (negative indices allowed!)
-	u = (x-1)
-	l = (y-1)
-
-	depth = 1 if base.ndim == 2 else base.shape[2]
-
-	t = time()
-	v  = (   xf  *    yf ).repeat(depth).reshape(base.shape) * base[x,y] \
-	   + ((1-xf) *    yf ).repeat(depth).reshape(base.shape) * base[u,y] \
-	   + (   xf  * (1-yf)).repeat(depth).reshape(base.shape) * base[x,l] \
-	   + ((1-xf) * (1-yf)).repeat(depth).reshape(base.shape) * base[u,l]
-
-	return (v, fmt)
-
-
-def iterate(f, x) :
-	yield x
-	while 1:
-		x = f(x)
-		yield x
-
-
-base_temp = None
-fmt_temp = None
-
-def zoomed_smooth_noise_helper(zoom) :
-	global base_temp
-	global fmt_temp
-	return zoomed_smooth_noise(base=base_temp, zoom=zoom, fmt=fmt_temp)[0]
-
-
-def turbulence(**kwargs) :
-
-	from itertools import takewhile, accumulate
-	from multiprocessing import Pool
-
-	base = kwargs['base']
-	zoom = kwargs['zoom']
-	fmt = kwargs['fmt']
-
-	izoom = zoom
-	zooms = list(takewhile(lambda x : x >= 1, iterate(lambda x : x/2, zoom)))
-
-	global base_temp
-	global fmt_temp
-	base_temp = base
-	#print(base)
-	fmt_temp = fmt
-	#from time import time
-	#t = time()
-	#with Pool(len(zooms)) as p :
-	#	vs = p.map(zoomed_smooth_noise_helper, zooms)
-	#v = sum(v*zooms[i] for (i,v) in enumerate(vs))
-
-	#print('Multiprocessing time: ', round(time() - t, 2))
-
-	#t = time()
-	v = np.zeros(base.shape, dtype=np.float)
-	while zoom >= 1 :
-		v += zoomed_smooth_noise(base=base, zoom=zoom, fmt=fmt)[0] * zoom
-		zoom /= 2
-	#print('Loop time: ', round(time() - t, 2))
-
-
-	return (v / (2*izoom), fmt)
-
-
-def arr_to_blues(arr) :
-	narr = arr
-	# go from grayscale to RGB
-	# R = 0, G = 0
-	arr  = np.full((*arr.shape, 3), 0, dtype=np.uint8)
-	# set blue (invert)
-	arr[...,2] = narr
-	return npqt.arr_to_image(arr, color.rgb888)
-
-
-def marble_base(**kwargs) :
-
-	R,C = kwargs['R'], kwargs['C']
-
-	arr = np.indices((R,C))
-	xy = arr[0] + arr[1]
-	return (128*(np.sin(xy / np.sqrt(max(R,C)))+1), color.gray8)
-
-def marble_true(**kwargs) :
-
-	base = kwargs['base']
-	fmt = kwargs['fmt']
-	R,C = base.shape[:2]
-	x,y=np.indices((R,C))
-	depth = 1 if len(base.shape) == 2 else base.shape[2]
-
-	# period of sinusoidal
-	xp = 5.0
-	yp = 10.0
-
-	# turbulence power
-	power = 5.0
-	zoom = 64.0
-
-	xy = (x * xp / R + y * yp / C).repeat(depth).reshape(base.shape) \
-	     + power * turbulence(zoom=zoom, base=base,fmt=fmt)[0] / 256
-	v = 256 * np.abs(np.sin(xy * np.pi))
-
-	return (v, fmt)
-
-def wood_base(**kwargs) :
-
-	R,C= kwargs['R'], kwargs['C']
-
-	arr = np.indices((R,C), dtype=np.float)
-	# move origin to center
-	arr[0] -= R / 2
-	arr[1] -= C / 2
-
-	return (128 * (np.sin(np.sqrt(arr[0] ** 2 + arr[1] ** 2) / np.sqrt(R+C))+1),
-          color.gray8)
-
-def wood(**kwargs) :
-
-	base = kwargs['base']
-	fmt = kwargs['fmt']
-
-	period = 12.0 # number of rings
-	power = 0.05 # twists
-	zoom = 32.0 # turbulence detail
-
-	R,C = base.shape
-	x,y = np.indices((R,C))
-
-	x = (x - R / 2) / R
-	y = (y - C / 2) / C
-	d = np.sqrt(x**2 + y**2) + power * turbulence(zoom=zoom, base=base,
-	fmt=fmt)[0] / 256
-	return (128 * np.abs(np.sin(2 * period * d * np.pi)), fmt)
-
-def weird_base(**kwargs) :
-
-	R,C = kwargs['R'], kwargs['C']
-	arr = np.indices((R,C))
-
-	f = 64*(np.sin(arr[0] / np.pi**2)+1 + np.sin(arr[1] / np.pi**2)+1)
-
-	return (f, color.gray8)
-
-def weird(**kwargs) :
-
-	base = kwargs['base']
-	fmt = kwargs['fmt']
-
-	zoom = 32
-	power = 128
-
-	R,C = base.shape
-	x,y = np.indices((R,C), dtype=np.float)
-
-	turb = turbulence(zoom=zoom, base=base, fmt=fmt)[0]
-	x += power * turb / 256
-	y += power * turb[::-1,::-1] / 256 # ???
-
-	f = 64*(np.sin(x / np.pi**2)+1 + np.sin(y / np.pi**2)+1)
-
-	return (f, fmt)
-
+	def populateLayout(self) :
+		self.layout.addWidget(self.textureDisplay)
+		self.layout.addWidget(self.paramGui_frame)
 
 if __name__ == '__main__' :
-
+	app = QtWidgets.QApplication([])
 	window = Main()
-
 	app.exec_()
-
